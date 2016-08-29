21704e83adf265ca48304248096837cba2a32551
Misael Saenz Flores <misa2pac@gmail.com>
2016-08-28 22:18:28 -0500

    Se agrego una nueva operacion
    Se agrego la operacion resta, esta es muy parecida a la
    funcion de sumar unicamente se ha cambiado la suma por
    la resta.
    
    Se planea en un futuro cambiarla por una funcion.

--- my_first_calculator.py
+++ my_first_calculator.py
@@ -2,15 +2,40 @@
 # TODO: Make it work for all floating point numbers too
 # Editada por Misael Saenz Flores
 
-BANDERA = False
+BANDERA = True
+RES = 0.0
 
-print("=================================")
-print("===========Calculadora===========")
-print("=================================")
-print("||  + Suma                     ||")
-print("||  - Resta                    ||")
-print("||  / Division                 ||")
-print("||  r Raiz                     ||")
-print("||  = Resutado                 ||")
-print("||  s Salir                    ||")
-print("=================================")
+while (True):
+    print("=================================")
+    print("===========Calculadora===========")
+    print("=================================")
+    print("||  + Suma                     ||")
+    print("||  - Resta                    ||")
+    print("||  / Division                 ||")
+    print("||  r Raiz                     ||")
+    print("||  = Resutado                 ||")
+    print("||  s Salir                    ||")
+    print("=================================")
+    opcion = input("Escribe una opcion: ")
+    if(opcion == '+'):
+        if(BANDERA):
+            OP1 = int(input("Dame operador 1: "))
+            BANDERA = False
+            if(BANDERA == False):
+                OP2 = int(input("Dame operador 2: "))
+                RES = OP1 + OP2
+    elif(opcion == '-'):
+        if(BANDERA):
+            OP1 = int(input("Dame operador 1: "))
+            BANDERA = False
+            if(BANDERA == False):
+                OP2 = int(input("Dame operador 2: "))
+                RES = OP1 - OP2            
+    elif(opcion == '='):
+        if(BANDERA):
+            print("Nada que mostrar!")
+        else:
+            print("El resultado es: " + str(RES))
+            BANDERA = True
+            RES = 0.0
+        pass        
