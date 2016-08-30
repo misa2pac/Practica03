39049f1492e4656a39a84256d394085c36b07022
Misael Saenz Flores <misa2pac@gmail.com>
2016-08-30 16:45:01 -0500

    Se agregaron mas funciones
    Se agrego la operacion divicion, ademas de la operacion raiz, la cual esta saca la raiz cuadrada de cualquier numero, es esta caso importamos la biblioteca math.

--- my_first_calculator.py
+++ my_first_calculator.py
@@ -1,3 +1,4 @@
+import math
 # my_first_calculator.py by AceLewis
 # TODO: Make it work for all floating point numbers too
 # Editada por Misael Saenz Flores
@@ -30,7 +31,20 @@ while (True):
             BANDERA = False
             if(BANDERA == False):
                 OP2 = int(input("Dame operador 2: "))
-                RES = OP1 - OP2            
+                RES = OP1 - OP2
+    elif(opcion == '/'):
+        if(BANDERA):
+            OP1 = int(input("Dame operador 1: "))
+            BANDERA = False
+            if(BANDERA == False):
+                OP2 = int(input("Dame operador 2: "))
+                RES = OP1/OP2
+    elif(opcion == 'r'):
+        if (BANDERA):
+            OP1 = int(input("Dame operador: "))
+            BANDERA = False
+            if(BANDERA == False):
+                RES = math.sqrt(OP1)
     elif(opcion == '='):
         if(BANDERA):
             print("Nada que mostrar!")
